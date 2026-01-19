46bd039619b1280fd21609d96bb9d423ad46d4ee
amelbnsm <la237329@student.helha.be>
2026-01-19 11:57:14 +0100

    implémentation

--- src/main/java/com/helha/backend/Infrastructure/folder/IFolderRepository.java
+++ src/main/java/com/helha/backend/Infrastructure/folder/IFolderRepository.java
@@ -1,4 +1,12 @@
 package com.helha.backend.Infrastructure.folder;
 
-public class IFolderRepository {
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public class IFolderRepository extends CrudRepository<DbFolder, Long>{
+    // récupère tous les dossiers d'un utilisateur
+    List<DbFolder> findByUserId(long userId);
+
+    // récupère tous les sous-dossiers d'un dossier donné
+    List<DbFolder> findByParentId(long parentId);
 }
