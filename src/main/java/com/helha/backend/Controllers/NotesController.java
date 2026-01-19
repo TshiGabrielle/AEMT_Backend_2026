59e398aa59d0d0d43f716fc1b418002964ef14c6
TshiGabrielle <la225722@student.helha.be>
2026-01-19 14:47:19 +0100

    Endpoint pour créer une note

--- src/main/java/com/helha/backend/Controllers/NotesController.java
+++ src/main/java/com/helha/backend/Controllers/NotesController.java
@@ -1,5 +1,33 @@
 package com.helha.backend.Controllers;
 
+import com.helha.backend.Application.notes.create.CreateNoteHandler;
+import com.helha.backend.Application.notes.create.CreateNoteInput;
+import com.helha.backend.Application.notes.create.CreateNoteOutput;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notes")
+@CrossOrigin // pour React (Vite)
+
 public class NotesController {
+
+    private final CreateNoteHandler createNoteHandler;
+
+
+    public NotesController(
+            CreateNoteHandler createNoteHandler
+    ) {
+        this.createNoteHandler = createNoteHandler;
+
+    }
+
+    // ---------------- CREATE NOTE ----------------
+    @PostMapping
+    public CreateNoteOutput create(@RequestBody CreateNoteInput input) {
+        return createNoteHandler.handle(input);
+    }
+
 
 }
