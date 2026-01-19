32f7f974e5455c40ef1224fe001871fadfe9fe79
amelbnsm <la237329@student.helha.be>
2026-01-19 12:59:35 +0100

    implémentation output/input

--- src/main/java/com/helha/backend/Application/notes/update/UpdateNoteInput.java
+++ src/main/java/com/helha/backend/Application/notes/update/UpdateNoteInput.java
@@ -1,4 +1,19 @@
 package com.helha.backend.Application.notes.update;
 
+// données nécessaires pour modifier une note
 public class UpdateNoteInput {
+    // id de la note à modifier
+    public long id;
+
+    // titre (ou nouveau titre) de la note
+    public String title;
+
+    // nouveau contenu en markdown
+    public String contentMarkdown;
+
+    public UpdateNoteInput(long id, String title, String contentMarkdown) {
+        this.id = id;
+        this.title = title;
+        this.contentMarkdown = contentMarkdown;
+    }
 }
