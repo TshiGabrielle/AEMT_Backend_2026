c4c791f1f9c3c06fe0589acd0e57c1ae17875734
amelbnsm <la237329@student.helha.be>
2026-01-19 12:34:28 +0100

    correction folder

--- src/main/java/com/helha/backend/Infrastructure/folder/IFolderRepository.java
+++ src/main/java/com/helha/backend/Infrastructure/folder/IFolderRepository.java
@@ -1,9 +1,13 @@
 package com.helha.backend.Infrastructure.folder;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-public class IFolderRepository extends CrudRepository<DbFolder, Long>{
+@Repository
+public interface IFolderRepository extends CrudRepository<DbFolder, Long> {
+
     // récupère tous les dossiers d'un utilisateur
     List<DbFolder> findByUserId(long userId);
 
