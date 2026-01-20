15eb8bb50519b62727356e396a3ab3ea9840b740
TshiGabrielle <la225722@student.helha.be>
2026-01-20 11:40:24 +0100

    Ajout du recalcul des métadonnées

--- src/main/java/com/helha/backend/Application/notes/update/UpdateNoteHandler.java
+++ src/main/java/com/helha/backend/Application/notes/update/UpdateNoteHandler.java
@@ -5,6 +5,7 @@ import com.helha.backend.Infrastructure.note.NoteRepository;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 // modifier une note existante
@@ -29,6 +30,18 @@ public class UpdateNoteHandler {
         // mettre à jour les champs principaux
         note.setName(input.name());
         note.setContent_markdown(input.contentMarkdown());
+
+
+        // Mise à jour de la date de modification
+        note.setUpdated_at(LocalDateTime.now());
+
+        // Recalcul des métadonnées
+        String content = input.contentMarkdown();
+        note.setNbcaract(content.length());
+        note.setNblines(content.isEmpty() ? 0 : content.split("\n").length);
+        note.setNbmots(content.isEmpty() ? 0 : content.split("\\s+").length);
+        note.setTaille_octet(content.getBytes().length);
+
 
         // générer le html depuis le markdown
         // (on copie juste tel quel pour l'instant)
